205bc6fc03be600fb43135e55c85bacdb8606a10
Liyons Varghese <liyonsvarghese@gmail.com>
2022-08-06 17:47:02 +0530

    DeathEffect

--- Assets/Scripts/Tank/Common/TankHealth.cs
+++ Assets/Scripts/Tank/Common/TankHealth.cs
@@ -10,9 +10,9 @@ public class TankHealth : MonoBehaviour
     public Color startColor=Color.green;
     public Color damageColor=Color.red;
     public float maxHealth = 100f;
-    public float currentHealth;
     public GameObject explosionPrefab;
 
+    private float currentHealth;
     private ParticleSystem explosionEffect;
     private bool isDead;
     public float sliderTimer;
@@ -28,7 +28,6 @@ public class TankHealth : MonoBehaviour
         currentHealth = maxHealth;
         isDead = false;
         SetHealthUI();
-
     }
     private void SetHealthUI()
     {
@@ -45,19 +44,51 @@ public class TankHealth : MonoBehaviour
         healthSlider.gameObject.SetActive(true);
         if(currentHealth<=0 && !isDead)
         {
+            isDead = true;
             OnDeath();
         }
     }
 
-    private void OnDeath()
+    public void OnDeath()
     {
-        isDead = true;
         explosionEffect.gameObject.SetActive(true);
         explosionEffect.gameObject.transform.position = transform.position;
         explosionEffect.Play();
+        if (gameObject.tag == "Player")
+        {
+            StartCoroutine(GameOver());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    IEnumerator GameOver()
+    {
+        yield return StartCoroutine(DestroyAllEnemies());
+        yield return new WaitForSeconds(1f);
+        yield return StartCoroutine(DestroyLevel());
         gameObject.SetActive(false);
     }
-
+    IEnumerator DestroyLevel()
+    {
+        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Level");
+        for (int i = 0; i < gameObjects.Length; i++)
+        {
+            yield return new WaitForSeconds(.2f);
+            Destroy(gameObjects[i]);
+        }
+    }
+    IEnumerator DestroyAllEnemies()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        for(int i=0;i<enemies.Length;i++)
+        {
+            TankHealth healthScript = enemies[i].GetComponent<TankHealth>();
+            healthScript.TakeDamage(healthScript.maxHealth);
+        }
+        yield return null;
+    }
     void SetInactive()
     {
         healthSlider.gameObject.SetActive(false);
